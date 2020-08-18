b25f1b7be223e36b0fdd6acf374f102464aac193
Parth Darji <22344168+erparthqa@users.noreply.github.com>
2020-08-18 17:21:07 +0530

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-navigator.serviceWorker.register('firebase-messaging-sw.js')
+navigator.serviceWorker.register('notification/firebase-messaging-sw.js')
   .then(function(registration) {
     console.log('Registration successful, scope is:', registration.scope);
   }).catch(function(err) {
