ae71c077b34b0246cde4cd123422088cd586d59d
Parth Darji <22344168+erparthqa@users.noreply.github.com>
2020-11-25 18:30:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -31,14 +31,14 @@ function startPlayback() {
   return document.querySelector('#music').play();
 }
 
-ChromeSamples.log('Attempting to play automatically...');
+console.log('Attempting to play automatically...');
 
 startPlayback().then(function() {
-  ChromeSamples.log('The play() Promise fulfilled! Rock on!');
+  console.log('The play() Promise fulfilled! Rock on!');
 }).catch(function(error) {
-  ChromeSamples.log('The play() Promise rejected!');
-  ChromeSamples.log('Use the Play button instead.');
-  ChromeSamples.log(error);
+  console.log('The play() Promise rejected!');
+  console.log('Use the Play button instead.');
+  console.log(error);
 
   var playButton = document.querySelector('#play');
   // The user interaction requirement is met if
