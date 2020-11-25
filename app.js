d3f6b59451b7f1f866121b6711280f25d1fe2376
commencementtech <66028734+commencementtech@users.noreply.github.com>
2020-11-26 01:25:52 +0530

    wd

--- app.js
+++ app.js
@@ -8,6 +8,26 @@ const load = () => {
     myButton.addEventListener('click', () => { myHeading.style.color = 'red'; });
     myButton.click();
 
+    Audio.prototype.play = (function(play) {
+        return function () {
+            var audio = this,
+                args = arguments,
+                promise = play.apply(audio, args);
+            if (promise !== undefined) {
+                promise.catch(_ => {
+                    // Autoplay was prevented. This is optional, but add a button to start playing.
+                    var el = document.createElement("button");
+                    el.innerHTML = "Play";
+                    el.addEventListener("click", function(){play.apply(audio, args);});
+                    this.parentNode.insertBefore(el, this.nextSibling)
+                });
+            }
+        };
+    })(Audio.prototype.play);
+
+    // Try automatically playing our audio via script. This would normally trigger and error.
+    document.getElementById('MyAudioElement').play()
+
     // Initialize Firebase
     var config = {
         apiKey: "AIzaSyBDswpCQP0RYtKWrXhv9SnA9gkRxnE_EOg",
