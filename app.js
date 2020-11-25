b3a6a331f839705b9c1f7f63d4266fb91d780845
commencementtech <66028734+commencementtech@users.noreply.github.com>
2020-11-26 00:57:57 +0530

    s

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-
-  // Initialize Firebase
-  var config = {
+// dJ-taXhudYX1Jg3PRxk7Ri:APA91bFclgj1wpgITNmUHQAA_sG56HLap5PsmJ-3CusZfvTofs0DlX5dmVO4agV_8kGUwLZSWsmZxVRQElzm2-eLX2eVam65mVqfmnoBxTSwFf8eR2ySSuaGHg4mwNnwCZDxnRxuz9i7
+// Initialize Firebase
+var config = {
     apiKey: "AIzaSyBDswpCQP0RYtKWrXhv9SnA9gkRxnE_EOg",
     authDomain: "web-test-462bb.firebaseapp.com",
     databaseURL: "https://web-test-462bb.firebaseio.com",
@@ -9,83 +9,69 @@
     messagingSenderId: "547115742706",
     appId: "1:547115742706:web:ac204f317be9dfa610676b",
     measurementId: "G-9RPKWD1XY8"
-  };
-  firebase.initializeApp(config);
+};
+firebase.initializeApp(config);
 
-  const messaging = firebase.messaging();
-  messaging.requestPermission().then(function() {
-     //getToken(messaging);
-     return messaging.getToken();
-  }).then(function(token){
-  console.log(token);
-  })
-.catch(function(err) {
-  console.log('Permission denied', err);
+const messaging = firebase.messaging();
+messaging.requestPermission().then(function () {
+    return messaging.getToken();
+}).then(function (token) {
+    console.log(token);
+})
+.catch(function (err) {
+    console.log('Permission denied', err);
 });
 
-function play_single_sound() {
-		document.getElementById('audiotag1').play();
-	}
+const source = "https://interactive-examples.mdn.mozilla.net/media/cc0-audio/t-rex-roar.mp3";
+const audio = new Audio(); // use the constructor in JavaScript, just easier that way
+audio.addEventListener("load", function() {
+    audio.play();
+}, true);
+audio.src = source;
+audio.autoplay = true;
+audio.loop = true;
+audio.volume = 1.0;
 
 function startPlayback() {
-  return document.querySelector('#music').play();
+    return document.querySelector('#music').play();
 }
 
-console.log('Attempting to play automatically...');
+startPlayback().then(function () {
+    console.log('The play() Promise fulfilled! Rock on!');
+}).catch(function (error) {
+    console.log('The play() Promise rejected!');
+    console.log('Use the Play button instead.');
+    console.log(error);
 
-startPlayback().then(function() {
-  console.log('The play() Promise fulfilled! Rock on!');
-}).catch(function(error) {
-  console.log('The play() Promise rejected!');
-  console.log('Use the Play button instead.');
-  console.log(error);
-
-  var playButton = document.querySelector('#play');
-  // The user interaction requirement is met if
-  // playback is triggered via a click event.
-  playButton.addEventListener('click', startPlayback);
-  playButton.hidden = false;
+    var playButton = document.querySelector('#play');
+    // The user interaction requirement is met if
+    // playback is triggered via a click event.
+    playButton.addEventListener('click', startPlayback);
+    playButton.hidden = false;
 });
 
-messaging.onMessage(function(payload){
-console.log('onMessage: ',payload);
-	startPlayback();
-  //$('#demo-center').html('<div class="mbsc-align-center mbsc-padding"><img src="https://img.mobiscroll.com/demos/logo-noshadow.jpg"><h4>Welcome on our website!</h4><p>Have fun navigating through the demos.</p></div>');
-  //alert(payload.data.call);
+messaging.onMessage(function (payload) {
+    console.log('onMessage: ', payload);
+    startPlayback();
+    //$('#demo-center').html('<div class="mbsc-align-center mbsc-padding"><img src="https://img.mobiscroll.com/demos/logo-noshadow.jpg"><h4>Welcome on our website!</h4><p>Have fun navigating through the demos.</p></div>');
+    //alert(payload.data.call);
 //   $('#ringme').click();
 // play_single_sound();
-  // WAVE SOUND TOGGLE
-//   var source = "https://interactive-examples.mdn.mozilla.net/media/cc0-audio/t-rex-roar.mp3";
-//   $.playSound(source);
-//     playSound(source);
-//   var audio = new Audio(); // use the constructor in JavaScript, just easier that way
-//   audio.addEventListener("load", function() {
-//       audio.play();
-//   }, true);
-//   audio.src = source;
-//   audio.autoplay = true;
-//   audio.loop = true;
-//   audio.volume = 1.0;
+    // WAVE SOUND TOGGLE
+    $('.audio').click();
 
-//   $('.audio').click();
-//   var playing = true;
-//   $('.audio').on('click', function(e) {
-//       if (playing == false) {
-//           audio.play();
-//           playing = true;
-
-//       } else {
-//           audio.pause();
-//           playing = false;
-//       }
-//   });
-  
 //   //centerDemo.show();
 //       $('#demo-center').show();
 //       return false;
 });
 
-function playSound(url){   
-  document.getElementById("sound").innerHTML="<embed src='"+url+"' hidden=true autostart=true loop=false>";
-} 
-
+let playing = true;
+$('.audio').on('click', function(e) {
+    if (playing == false) {
+        audio.play();
+        playing = true;
+    } else {
+        audio.pause();
+        playing = false;
+    }
+});
